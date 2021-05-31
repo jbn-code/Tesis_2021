1d2f8d1c67a4a5e2c04ebaac0fdb2251e2ad72e6
facunava <facunavaxyz@gmail.com>
2021-05-31 07:25:42 -0300

    Agrego Carpeta de Campo actualizada al 30/05/21

---
+++ 02_software/development/Heltec_sht11/src/configuration.h
@@ -0,0 +1,12 @@
+//Board: Hectec Lora Wifi LoRa V2
+
+#define I2C_SDA 4   // I2C pins for this board
+#define I2C_SCL 15  // I2C pins for this board
+
+#define RESET_OLED 16 // If defined, this pin will be used to reset the display controller
+#define LED_PIN 25     // If defined we will blink this LED
+#define BUTTON_PIN 0   // If defined, this will be used for user button presses
+
+#define data1Pin 22
+#define data2Pin 21 
+#define sclkPin  19
